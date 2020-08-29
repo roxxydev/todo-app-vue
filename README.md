254bb8adc3a074454cf3c34278f73ddd7fb6390f
Roxxy Rafael <roxxydev@gmail.com>
2020-08-29 15:40:19 +0800

    Initial Todo App using Vue.js

---
+++ README.md
@@ -0,0 +1,26 @@
+This project was created with [Vue CLI](https://cli.vuejs.org/).
+
+## Project setup
+```
+npm install
+```
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm run serve`
+This compiles and hot-reloads for development and runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### `npm run build`
+Compiles and minifies for production
+
+### `npm run lint`
+Lints and fixes files
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <Header/>
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:delete-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/header'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Watch',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Learn',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Code',
+          completed: true
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo (id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo (todo) {
+      this.todos = [...this.todos, todo];
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  color: #2c3e50;
+}
+
+.btn {
+  display: inline;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+   background: #666;
+}
+</style>
